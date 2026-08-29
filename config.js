885e3ec80064103f4ff89d6e10fbab74334c9e37
TubeY0u <190124375+TubeY0u@users.noreply.github.com>
2026-08-29 22:27:57 +0000

    Supabase-Projekt verbunden

--- config.js
+++ config.js
@@ -11,6 +11,5 @@
    alles bleibt im Browser des jeweiligen Geräts.
 ------------------------------------------------------------------- */
 window.RR_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
-};
+  SUPABASE_URL: "https://saggutamgsoqjqztccog.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhZ2d1dGFtZ3NvcWpxenRjY29nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgwMzMwMzMsImV4cCI6MjEwMzYwOTAzM30.A0rkI34rqH0g2lSYJ39pz-PcQ5tYbetuUfX7L2ibIg4"};
